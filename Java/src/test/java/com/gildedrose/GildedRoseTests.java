7cb5487b9cde1b2b1db218a60307ee24bfebeb6a
Yoan Thirion <yoan.thirion@gmail.com>
2020-08-04 19:08:07 +0200

    Add CombinationApprovals

--- Java/src/test/java/com/gildedrose/GildedRoseTests.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTests.java
@@ -1,15 +1,28 @@
 package com.gildedrose;
 
-import org.approvaltests.Approvals;
+import org.approvaltests.combinations.CombinationApprovals;
 import org.junit.jupiter.api.Test;
 
 public class GildedRoseTests {
     @Test
     public void updateQuality() {
-        var items = new Item[]{new Item("a common item", 0, 0)};
-        var gildedRose = new GildedRose(items);
+        var name = "a common item";
+        var sellIn = 0;
+        var quality = 0;
 
+        CombinationApprovals.verifyAllCombinations(
+                this::callUpdateQuality,
+                new String[]{name},
+                new Integer[]{sellIn},
+                new Integer[]{quality}
+        );
+    }
+
+    private String callUpdateQuality(String name, int sellIn, int quality) {
+        var items = new Item[]{new Item(name, sellIn, quality)};
+        var gildedRose = new GildedRose(items);
         gildedRose.updateQuality();
-        Approvals.verify(gildedRose.items[0]);
+
+        return gildedRose.items[0].toString();
     }
 }
