c048d56dd4d046eb0c2db7b3668b0a6104cce51c
Yoan Thirion <yoan.thirion@gmail.com>
2020-08-04 19:32:35 +0200

    100% coverage

--- Java/src/test/java/com/gildedrose/GildedRoseTests.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTests.java
@@ -8,9 +8,12 @@ public class GildedRoseTests {
     public void updateQuality() {
         CombinationApprovals.verifyAllCombinations(
                 this::callUpdateQuality,
-                new String[]{"a common item"},
-                new Integer[]{0},
-                new Integer[]{0}
+                new String[]{"a common item",
+                        "Aged Brie",
+                        "Backstage passes to a TAFKAL80ETC concert",
+                        "Sulfuras, Hand of Ragnaros"},
+                new Integer[]{-1, 0, 11},
+                new Integer[]{0, 1, 49, 50}
         );
     }
 
